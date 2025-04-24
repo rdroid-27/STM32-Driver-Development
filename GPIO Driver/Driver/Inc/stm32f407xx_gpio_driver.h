f9668721d914e7fbf9ee368614b24137bbd0f2d2
rdroid-27 <gupta27rachit@gmail.com>
2025-04-24 22:57:02 +0530

    Add GPIO driver code with structures definitions and API prototypes.

---
+++ GPIO Driver/Driver/Inc/stm32f407xx_gpio_driver.h
@@ -0,0 +1,140 @@
+/*
+ * stm32f407xx_gpio_driver.h
+ *
+ *  Created on: Apr 24, 2025
+ *      Author: GANDALF
+ */
+
+#ifndef INC_STM32F407XX_GPIO_DRIVER_H_
+#define INC_STM32F407XX_GPIO_DRIVER_H_
+
+#include "stm32f407xx.h"
+
+// <------------------------------------------------------------------------------>
+
+// Pin Configuration Structure for GPIO Pin
+/* This structure is used to hold all the settings needed to configure a single GPIO pin. Instead of setting each value separately, we put all the configurations into one structure for easy management and cleaner code.
+ */
+typedef struct
+{
+    uint8_t GPIO_PinNumber;
+    uint8_t GPIO_PinMode;
+    uint8_t GPIO_PinSpeed;
+    uint8_t GPIO_PinPuPdControl;
+    uint8_t GPIO_PinOPType;
+    uint8_t GPIO_PinAltFuncMode;
+} GPIO_PinConfig_t;
+
+// <------------------------------------------------------------------------------>
+
+// Handle Structure for a GPIO Pin
+/* This structure is used to group everything needed to initialize and use a GPIO pin.
+ */
+typedef struct
+{
+    GPIO_RegDef_t *pGPIOx;
+    GPIO_PinConfig_t GPIO_PinConfig;
+} GPIO_Handle_t;
+
+// <------------------------------------------------------------------------------>
+
+// Enable The GPIO port by enable the corresponding peripheral clock associated with it and vice versa for disabling the GPIO port.
+void GPIO_PeriClkControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
+{
+    if (EnorDi == ENABLE)
+    {
+        if (pGPIOx == GPIOA)
+        {
+            GPIOA_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOB)
+        {
+            GPIOB_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOC)
+        {
+            GPIOC_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOD)
+        {
+            GPIOD_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOE)
+        {
+            GPIOE_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOF)
+        {
+            GPIOF_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOG)
+        {
+            GPIOG_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOH)
+        {
+            GPIOH_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOI)
+        {
+            GPIOI_PCLK_EN();
+        }
+    }
+    else if (EnorDi == DISABLE)
+    {
+        if (pGPIOx == GPIOA)
+        {
+            GPIOA_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOB)
+        {
+            GPIOB_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOC)
+        {
+            GPIOC_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOD)
+        {
+            GPIOD_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOE)
+        {
+            GPIOE_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOF)
+        {
+            GPIOF_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOG)
+        {
+            GPIOG_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOH)
+        {
+            GPIOH_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOI)
+        {
+            GPIOI_PCLK_DI();
+        }
+    }
+}
+
+// Driver APIs
+
+void GPIO_Init(GPIO_Handle_t *pGPIO_Handle);
+void GPIO_Deinit(GPIO_RegDef_t *pGPIOx);
+
+void GPIO_PeriClkControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi);
+
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber);
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx);
+void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber, uint8_t value);
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t value);
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber);
+
+void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi);
+void GPIO_IRQHandling(uint8_t pinNumber);
+
+#endif /* INC_STM32F407XX_GPIO_DRIVER_H_ */
